fad33081c96a90dbe4b6eca2dff9d9340e70fa04
Michiko Inoue <inouem888@gmail.com>
2024-12-03 20:10:09 +0000

    Implement edit and delete functionality for comments

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from .import views
 urlpatterns = [
     path('', views.PostList.as_view(), name='blog'),
     path('<int:id>/', views.blog_detail, name='blog_detail'),
+    path('<int:id>/comment/<int:comment_id>/edit/', views.comment_edit, name='comment_edit'),
+    path('<int:id>/comment/<int:comment_id>/delete/', views.comment_delete, name='comment_delete'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from django.contrib import messages
-from .models import Post
+from django.http import HttpResponseRedirect
+from .models import Post, Comment
 from .forms import CommentForm
 
 
@@ -15,12 +16,14 @@ class PostList(generic.ListView):
     context_object_name = 'posts'
     paginate_by = 6
 
+
+
 def blog_detail(request, id):
     """
     Displays a single blog post along with its comments and a comment form.
     """
     blog = get_object_or_404(Post, id=id)
-    comments = blog.comments.all().order_by("-created_on")
+    comments = blog.comments.all().order_by("-created_on")  
     comment_count = comments.count()
 
     if request.method == "POST":
@@ -30,8 +33,8 @@ def blog_detail(request, id):
             comment.author = request.user
             comment.post = blog
             comment.save()
-            messages.success(request, "Your comment has been added successfully.")
-            return redirect('blog_detail', id=blog.id)         
+            messages.success(request, "Your comment has been added successfully.") 
+            return redirect('blog_detail', id=blog.id)  
 
     else:
         comment_form = CommentForm()
@@ -46,4 +49,57 @@ def blog_detail(request, id):
             'comment_form': comment_form,
         }
     )
+
+
+def comment_edit(request, id, comment_id):
+    """
+    Edit a specific comment directly on the blog detail page.
+    """
+    blog = get_object_or_404(Post, id=id)
+    comment = get_object_or_404(Comment, id=comment_id)
+
+    if comment.author != request.user:
+        messages.error(request, "You are not authorized to edit this comment.")
+        return redirect('blog_detail', id=blog.id)
+
+    if request.method == "POST":
+        comment_form = CommentForm(request.POST, instance=comment)
+        if comment_form.is_valid():
+            comment_form.save()
+            messages.success(request, "Your comment has been successfully updated.")
+            return redirect('blog_detail', id=blog.id)
+        else:
+            messages.error(request, "There was an error updating your comment.")
+    else:
+        comment_form = CommentForm(instance=comment)
+
+    # Pass the edit_comment instance to the template
+    return render(
+        request,
+        'blog/blog_detail.html',
+        {
+            'blog': blog,
+            'comments': blog.comments.all().order_by("-created_on"),
+            'comment_form': comment_form,
+            'edit_comment': comment,
+        }
+    )
+
+
+
+def comment_delete(request, id, comment_id):
+    """
+    Delete a specific comment.
+    """
+    blog = get_object_or_404(Post, id=id)
+    comment = get_object_or_404(Comment, id=comment_id)
+
+    if comment.author == request.user:  # Check if the current user is the comment's author
+        comment.delete()
+        messages.success(request, "Your comment has been successfully deleted.")
+    else:
+        messages.error(request, "You are not authorized to delete this comment.")
+    return redirect('blog_detail', id=blog.id)
+
+
 
