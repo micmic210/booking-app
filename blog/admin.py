87287bb6db677b2507a3b384e2b953be794c59e1
Michiko Inoue <inouem888@gmail.com>
2024-12-03 11:43:42 +0000

    Implemented blog post and comment models, added admin configurations

--- blog/admin.py
+++ blog/admin.py
@@ -4,10 +4,22 @@ from .models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for blog posts
+    """
     list_display = ('title', 'author', 'status', 'created_on')
     list_filter = ('status', 'author', 'created_on')
     search_fields = ('title', 'content')
     ordering = ['-created_on']
 
-admin.site.register(Comment) 
+@admin.register(Comment) 
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for comments
+    """
+    list_display = ('author', 'post', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ('author__username', 'body')
+
+
 
