637ae9ee2b094909a0e2b99cc1c2720248150cd2
Michiko Inoue <inouem888@gmail.com>
2024-12-04 14:39:52 +0000

    Add verbose_name_plural categories to blog models.py

--- blog/models.py
+++ blog/models.py
@@ -65,5 +65,9 @@ class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
 
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories" 
+
     def __str__(self):
         return self.name
