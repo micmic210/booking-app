60b5b239e9fdfec0aadf784267de230067762379
Michiko Inoue <inouem888@gmail.com>
2024-12-05 11:58:12 +0000

    Modify views.py and menu.html to show the images and info registerd in admin panel

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Menu
 
 def menu_view(request):
-    return render(request, 'menu/menu.html')
+    ramen_items = Menu.objects.filter(category='ramen', is_available=True)
+    starters_items = Menu.objects.filter(category='starters', is_available=True)
+    appetizers_items = Menu.objects.filter(category='appetizers', is_available=True)
+
+    return render(request, 'menu/menu.html', {
+        'ramen_items': ramen_items,
+        'starters_items': starters_items,
+        'appetizers_items': appetizers_items,
+    })
+
