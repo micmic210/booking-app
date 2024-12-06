fbe231c5011e91aae0aef794d5d2087ab91d36e6
Michiko Inoue <inouem888@gmail.com>
2024-12-06 12:49:49 +0000

    Set DEBUG to False for deploy

--- my_project/settings.py
+++ my_project/settings.py
@@ -36,7 +36,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-micmic210-bookingapp-ksnq98m924i.ws.codeinstitute-ide.net',
