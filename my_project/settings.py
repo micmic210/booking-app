1042d399c7d3fc22cd32beae913e16d0ab878cbb
Michiko Inoue <inouem888@gmail.com>
2024-12-06 16:05:04 +0000

    Remove unnecessary old booking-app folder and adjust the database URL setting

--- my_project/settings.py
+++ my_project/settings.py
@@ -106,13 +106,29 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-DATABASES = {
-    'default': dj_database_url.parse(
-        str(os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3")),  # デフォルトはSQLite
-        conn_max_age=600,  
-        ssl_require=True   
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.parse(
+#         str(os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3")),  # デフォルトはSQLite
+#         conn_max_age=600,  
+#         ssl_require=True   
+#     )
+# }
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 # CSRF Settings
 CSRF_TRUSTED_ORIGINS = [
