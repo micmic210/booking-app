6dbce4bac21c67a5065927c7f77e5a25d4764548
Michiko Inoue <inouem888@gmail.com>
2024-12-07 13:24:38 +0000

    Remove subscribe.html and related views

--- my_project/settings.py
+++ my_project/settings.py
@@ -62,7 +62,6 @@ INSTALLED_APPS = [
     'menu',
     'contact',
     'blog',
-    'newsletter',
 ]
 
 SITE_ID = 1
@@ -108,7 +107,7 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 # DATABASES = {
 #     'default': dj_database_url.parse(
-#         str(os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3")),  # デフォルトはSQLite
+#         str(os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3")),  
 #         conn_max_age=600,  
 #         ssl_require=True   
 #     )
@@ -156,18 +155,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
-
-#Email Configuration for SendGrid
-# Email Backend
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-# SendGrid SMTP Server
-# EMAIL_HOST = 'smtp.sendgrid.net'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'apikey'  
-# EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_API_KEY')  
-# DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')  
 
 
 # Internationalization
